48b320892098622772ddb244e4fab2e38c04fbc0
jcpwfloi <jcpwfloi@gmail.com>
2014-11-23 20:34:24 +0800

    problems fetch completed

--- model/problem.js
+++ model/problem.js
@@ -1,2 +1,29 @@
 var mongo = require('mongodb');
 var server = new mongo.Server('127.0.0.1', 27017);
+var mongodb = new mongo.Db('cboj', server, {safe: true});
+
+function fetchProblems(from, num, callback) {
+    from += 1000;
+    mongodb.open(function(err, db) {
+        if (err) callback(err, null);
+        db.collection('problems', function(err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err, null);
+            }
+            collection.find({id: {$gte: from, $lt: from + num}}, function(err, doc) {
+                if (err) {
+                    mongodb.close();
+                    return callback(err, null);
+                }
+                doc.toArray(function(err, arr) {
+                    mongodb.close();
+                    if (err) callback(err, null);
+                    callback(err, arr);
+                });
+            });
+        });
+    });
+}
+
+exports.fetchProblems = fetchProblems;
--- routes/problem.js
+++ routes/problem.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var p = require('../model/problem');
 
 var nav = [
 	{ name: '首页', ref: '/', active: true },
@@ -9,23 +10,30 @@ var nav = [
 	{ name: '排名', ref: '/ranklist', active: false }
             ];
 
-var problems = [ 
-    { styl: 'success', stat: 'Y', id: '1', name: 'a + b Problem', all: 1000, ac: 1000, rat: '100%' },
-];
+var problems; 
 
-var perPage = 40;
+var perPage = 50;
 
-function getProblems(pageId) {
+function getProblems(pageId, callback) {
+    console.log(pageId);
+    p.fetchProblems(1 + perPage * (pageId - 1), perPage, function(err, doc) {
+        if (err) return;
+        problems = doc;
+        console.log(doc);
+        callback();
+    });
 }
 
 router.get('/', function(req, res) {
-    getProblems(1);
-    res.render('problem', { title: '问题 - CodeBursts', nav: nav, problems: problems });
+    getProblems(1, function() {
+        res.render('problem', { title: '问题 - CodeBursts', nav: nav, problems: problems });
+    });
 });
 
 router.get('/:pageId', function(req, res) {
-    getProblems(req.params.pageId);
-    res.render('problem', { title: '问题 - CodeBursts', nav: nav, problems: problems });
+    getProblems(req.params.pageId, function() {
+        res.render('problem', { title: '问题 - CodeBursts', nav: nav, problems: problems });
+    });
 });
 
 module.exports = router;
