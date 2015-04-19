4b47b5df42c4c7ff7007a0aa45470450ec936070
jcpwfloi <jcpwfloi@gmail.com>
2015-04-19 19:49:43 +0800

    small details upgrade

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,6 @@ router.get('/', function(req, res) {
     if (req.session.user) {
         login = req.session.user;
     }
-    console.log(login);
     var passdata = 
     {
       title: '首页 - CodeBursts!',
